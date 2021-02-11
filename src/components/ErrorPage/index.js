4c6a0aacdc25e7eb9bd97bb7b29855e3c76fd3a1
Hilderal Térence <terencehilderal@gmail.com>
2021-02-11 22:56:50 +0100

    styling error page

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Batman from'../../images/batman.png'
 
 const ErrorPage = () => {
   return (
     <div className="quiz-bg">
       <div className="container">
-        SIGNUP
+        <h2>Oups, this page doesn't exist</h2>
+        <img src={Batman} alt="errorpage"/>
       </div>
     </div>
   )
