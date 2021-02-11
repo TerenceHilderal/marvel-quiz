e50a5e6d1d697580e6385780784b9b07702f42f2
Hilderal Térence <terencehilderal@gmail.com>
2021-02-11 21:46:42 +0100

    display button inscription and connexion after 1s

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="footer-container">
-        <p>Projet Réalisé par Master972-2020</p>
-        <p>Les icônes wolverine,batman,iron-man,spiderman sont prises sur iconfinder.com</p>
-      </div>
-    </footer>
-
-  )
+	return (
+		<footer>
+			<div className='footer-container'>
+				<p>Projet Réalisé par Master972-2020</p>
+				<p>
+					Les icônes wolverine,batman,iron-man,spiderman sont prises sur
+					iconfinder.com
+				</p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
