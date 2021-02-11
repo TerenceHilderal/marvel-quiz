4bc15bc6655363359d8b785a7a7cedca95ef250e
Hilderal Térence <terencehilderal@gmail.com>
2021-02-11 21:15:07 +0100

    setup project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet Réalisé par Master972-2020</p>
+        <p>Les icônes wolverine,batman,iron-man,spiderman sont prises sur iconfinder.com</p>
+      </div>
+    </footer>
+
+  )
+}
+
+export default Footer
